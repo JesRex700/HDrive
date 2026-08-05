3df95bb1425712822890e8de843bdd2340391dcf
Jesus Amezquita <thomasjesus70@gmail.com>
2026-08-05 00:38:32 -0600

    Update type_1_test.ino

--- examples/IO_Testing/type_1_test/type_1_test.ino
+++ examples/IO_Testing/type_1_test/type_1_test.ino
@@ -1,7 +1,7 @@
 #include "hdrive.h"
 
-drive drive(5, 6, 7, 8, 0);
-
+drive drive(5, 6, 7, 8, 0); //The zero on the end is an idetifier, it means that you are not using PWM on the inputs (You can use any number).
+                            //If removed, it will send PWM signals to the inputs and change to a type 3 driver.
 void setup() {
 
   drive.init();
