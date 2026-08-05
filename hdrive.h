b202fe5510d46943bf92f028cbe15bc04688903a
Jesus Amezquita <thomasjesus70@gmail.com>
2026-08-04 23:59:09 -0600

    Update hdrive.h to 2.0.0 (beta)
    - Added 4 more constructors
    - Added an Identificator for each motor type
    - Added support for Enable chaneled PWM
    - Added support for Input chaneled PWM
    - Updated every movement function to work with the new supported drivers
    - Changed "freerun" to "sleep"
    - Changed "enbrun" to "wakeUp"
    - Changed "embrake" to "brake"
    - Eliminated function "setPwm" because the new constructors already do the same function.
    
    - Beta version, not tested yet

--- hdrive.h
+++ hdrive.h
@@ -8,6 +8,7 @@ compatible con funciones de PWM, freno
 de emergerncia y giro libre de la rueda.
 Limitado al control de solo dos motores
 de forma directa por ahora.
+            Versión 2.0.0
 
          ---HDRIVE LIBRARY---
 BY: Jesus Amezquita, KAME ELECTRONICS
@@ -20,7 +21,7 @@ Limited for the moment at two motor direct
 control.
 
 ©Jesus Amezquita ©Kame Electronics
-v.1.1.0
+              Version 2.0.0
 */
 #ifndef _HDRIVE_H
 #define _HDRIVE_H
@@ -36,19 +37,30 @@ private:
   byte pwmA;
   byte pwmB;
   byte stby;
+  byte inp1;
+  byte inp2;
+  byte inp3;
+  byte inp4;
+  byte en1;
+  byte en2;
+  int type;
+  int idf;
 public:
-  drive(byte stby, byte motA1, byte motA2, byte motB1, byte motB2);
+  drive(byte motA1, byte motA2, byte motB1, byte motB2, byte stby, byte pwmA, byte pwmB);
+  drive(byte motA1, byte motA2, byte motB1, byte motB2, int idf = 0);
+  drive(byte motA1, byte motA2, byte motB1, byte motB2, byte en1, byte en2);
+  drive(byte motA1, byte motA2, byte motB1, byte motB2);
+  drive(byte motA1, byte motA2);
   void init();
   void off();
-  void setpwm(byte pwmA , byte pwmB);
   void forward(int pwmVa = 255, int pwmVb = 255);
   void backward(int pwmVa = 255, int pwmVb = 255);
   void left(int pwmVa = 255, int pwmVb = 255);
   void right(int pwmVa = 255, int pwmVb = 255);
-  void embrake(int pwmVa = 255, int pwmVb = 255);
-  void freerun();
-  void enbrun();
-  void prueba(int pwmVa = 255, int pwmVb = 255);
+  void brake(int pwmVa = 255, int pwmVb = 255);
+  void sleep();
+  void wakeUp();
+  void test(int pwmVa = 255, int pwmVb = 255);
 };
 
 #endif
