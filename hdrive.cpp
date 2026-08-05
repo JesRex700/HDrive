d55a46eb164e2296d765736deaccbab7e88ef8a6
Jesus Amezquita <thomasjesus70@gmail.com>
2026-08-04 23:56:18 -0600

    Update hdrive.cpp to 2.0.0 (beta)
    - Added 4 more constructors
    - Added an Identificator for each motor type
    - Added support for Enable chaneled PWM
    - Added support for Input chaneled PWM
    - Updated every movement function to work with the new supported drivers
    - Changed "freerun" to "sleep"
    - Changed "enbrun" to "wakeUp"
    - Eliminated function "setPwm" because the new constructors already do the same function.
    
    - Beta version, not tested yet

--- hdrive.cpp
+++ hdrive.cpp
@@ -1,12 +1,68 @@
 #include "hdrive.h"
 
-drive::drive(byte stby, byte motA1, byte motA2, byte motB1, byte motB2) {  //Esta seccion declara los pines que se usaran fisicamente en el microcontrolador para cada entrada del controlador; stby debe conectarse fisicamente a los 2 enable en caso de tenerlos el controlador a usar
-  this->motA1 = motA1;                                                     //Motor A, Input 1                                  //This secction declares the pins that will be used physically on the microcontroller for eachn of the drivers inputs; stby muist be connected physically to both enable pins in case of having them
-  this->motA2 = motA2;                                                     //Motor A, Input 2 //Motor Derecho / Right Motor
-  this->motB1 = motB1;                                                     //Motor B, Input 1
-  this->motB2 = motB2;                                                     //Motor B, Input 2 //Motor Izquierdo / Left Motor
-  this->stby = stby;                                                       //Stanby / Enable
+//  ---2 Motors, 1 Stanby/En, 2 Independent PWM Channels--
+//  ---2 Motores, 1 Standby/En, 2 Canales de PWM independientes---
+//  ---For: TB6612FNG or similars---
+//  ---Para: TB6612FNG o similares---
+
+drive::drive(byte stby, byte motA1, byte motA2, byte motB1, byte motB2, byte pwmA, byte pwmB) {
+  this->motA1 = motA1;  //Motor A, Input 1
+  this->motB1 = motB1;  //Motor B, Input 1
+  this->motB2 = motB2;  //Motor B, Input 2 //Motor Izquierdo / Left Motor
+  this->stby = stby;    //Stanby
+  this->pwmB = pwmB;    //Pin PWM Motor B
+  this->pwmA = pwmA;    //Pin PWM Motor A
+  this->type = 0;
 }
+
+//  ---2 Motors, 1 Stanby/En, No PWM---
+//  ---2 Motores, 1 Stanby/En, No PWM---
+//  ---For: L293D(MC enabled) or similars---
+//  ---Para: L293D(Activado mediante MC) o similares---
+
+drive::drive(byte motA1, byte motA2, byte motB1, byte motB2, int idf) {
+  this->motA1 = motA1;  //Motor A, Input 1
+  this->motA2 = motA2;  //Motor A, Input 2 //Motor Derecho / Right Motor
+  this->motB1 = motB1;  //Motor B, Input 1
+  this->motB2 = motB2;  //Motor B, Input 2 //Motor Izquierdo / Left Motor
+  this->idf = 0;    //Place holder
+  this->type = 1;
+}
+
+//  ---2 Motors, 2 Enable/Stby, Enable pins are also for PWM---
+//  ---2 Motores, 2 Enable/Stby, Los pines Enable tambien son para PWM---
+//  ---For: L298N or similars---
+//  ---Para: L298N o similares---
+
+drive::drive(byte inp1, byte inp2, byte inp3, byte inp4, byte en1, byte en2) {
+  this->inp1 = motA1;  //Input 1
+  this->inp2 = motA2;  //Input 2 //Motor Derecho / Right Motor
+  this->inp3 = motB1;  //Input 3
+  this->inp4 = motB2;  //Input 4 //Motor Izquierdo / Left Motor
+  this->en1 = en1;     //Enable 1
+  this->en2 = en2;     //Enable 2
+  this->type = 2;
+}
+
+//  ---2 Motors, No Stby/En, No PWM/ PWM via Input
+//  ---2 Motores, No Stby/En, No PWM/ PWM via Input
+//  ---For: DRV88XX or similars---
+//  ---Para: DRV88XX o similares---
+
+drive::drive(byte motA1, byte motA2, byte motB1, byte motB2) {
+  this->motA1 = motA1;  //Motor A, Input 1
+  this->motA2 = motA2;  //Motor A, Input 2 //Motor Derecho / Right Motor
+  this->motB1 = motB1;  //Motor B, Input 1
+  this->motB2 = motB2;  //Motor B, Input 2 //Motor Izquierdo / Left Motor
+  this->type = 3;
+}
+
+drive::drive(byte motA1, byte motA2) {
+  this->motA1 = motA1;  //Motor Input 1
+  this->motA2 = motA2;  //Motor Input 2
+  this->type = 4;
+}
+
 void drive::init() {       //Inicia todos los pines del microcontrolador asociados al controlador, estos estan pensados para arduino, otros microcontroladores tal vez requieran configuracion adicional
   pinMode(motA1, OUTPUT);  //Initialize all the microcontrolles driver ralated pins, this are designed for arduino, other microcontrollers might need extra configuration
   pinMode(motA2, OUTPUT);
@@ -15,67 +71,287 @@ void drive::init() {       //Inicia todos los pines del microcontrolador asociad
   pinMode(pwmA, OUTPUT);
   pinMode(pwmB, OUTPUT);
   pinMode(stby, OUTPUT);
+  pinMode(en1, OUTPUT);
+  pinMode(en2, OUTPUT);
   digitalWrite(stby, HIGH);
+  digitalWrite(en1, HIGH);
+  digitalWrite(en2, HIGH);
   off();
 }
-void drive::off() {
-  digitalWrite(motA1, LOW);
+
+void drive::off() {          //Apaga los motores
+  digitalWrite(motA1, LOW);  //Turns off the motors
   digitalWrite(motA2, LOW);
   digitalWrite(motB1, LOW);
   digitalWrite(motB2, LOW);
 }
-void drive::setpwm(byte pwmA, byte pwmB) {  //Activa la funcion pwm, si no se necesita dedbe dejarse fuera del codigo para evitar problemas
-  this->pwmA = pwmA;                        //Pin PWM Motor A      //Activates the pwm function, if not needed this must be left out to aviod problems
-  this->pwmB = pwmB;                        //Pin PWM Motor B
+
+void drive::forward(int pwmVa, int pwmVb) {  //Drive Forward / Avanzar hacia adelante
+  switch (type) {
+
+      //2 Motors, PWM via dedicated channel
+
+    case 0:
+      analogWrite(pwmA, pwmVa);
+      digitalWrite(motA1, HIGH);
+      digitalWrite(motA2, LOW);
+      analogWrite(pwmB, pwmVb);
+      digitalWrite(motB1, HIGH);
+      digitalWrite(motB2, LOW);
+      break;
+
+      //2 Motors, no PWM
+
+    case 1:
+      digitalWrite(motA1, HIGH);
+      digitalWrite(motA2, LOW);
+      digitalWrite(motB1, HIGH);
+      digitalWrite(motB2, LOW);
+      break;
+
+      //2 Motors, PWM via enable
+
+    case 2:
+      analogWrite(en1, pwmVa);
+      digitalWrite(motA1, HIGH);
+      digitalWrite(motA2, LOW);
+      analogWrite(en2, pwmVb);
+      digitalWrite(motB1, HIGH);
+      digitalWrite(motB2, LOW);
+      break;
+
+      //2 Motors, No PWM/ PWM via input
+
+    case 3:
+      analogWrite(motA1, pwmVa);
+      digitalWrite(motA2, LOW);
+      analogWrite(motB1, pwmVb);
+      digitalWrite(motB2, LOW);
+      break;
+
+      //1 Motor
+
+    case 4:
+      analogWrite(motA1, pwmVa);
+      digitalWrite(motA2, LOW);
+      break;
+  }
 }
-void drive::forward(int pwmVa, int pwmVb) {  //Movimiento frontal
-  analogWrite(pwmA, pwmVa);                  //Forwards movement
-  digitalWrite(motA1, HIGH);
-  digitalWrite(motA2, LOW);
-  analogWrite(pwmB, pwmVb);
-  digitalWrite(motB1, HIGH);
-  digitalWrite(motB2, LOW);
+
+void drive::backward(int pwmVa, int pwmVb) {  //Drive Backwards / Avanzar hacia atras
+  switch (type) {
+
+      //2 Motors, PWM via dedicated channel
+
+    case 0:
+      analogWrite(pwmA, pwmVa);
+      digitalWrite(motA1, LOW);
+      digitalWrite(motA2, HIGH);
+      analogWrite(pwmB, pwmVb);
+      digitalWrite(motB1, LOW);
+      digitalWrite(motB2, HIGH);
+      break;
+
+      //2 Motors, No PWM
+
+    case 1:
+      digitalWrite(motA1, LOW);
+      digitalWrite(motA2, HIGH);
+      digitalWrite(motB1, LOW);
+      digitalWrite(motB2, HIGH);
+      break;
+
+      //2 Motors, PWM via enable
+
+    case 2:
+      analogWrite(en1, pwmVa);
+      digitalWrite(motA1, LOW);
+      digitalWrite(motA2, HIGH);
+      analogWrite(en2, pwmVb);
+      digitalWrite(motB1, LOW);
+      digitalWrite(motB2, HIGH);
+      break;
+
+      //2 Motors, No PWM/ PWM via input
+
+    case 3:
+      analogWrite(motA1, LOW);
+      digitalWrite(motA2, pwmVa);
+      analogWrite(motB1, LOW);
+      digitalWrite(motB2, pwmVb);
+      break;
+
+      //1 Motor
+
+    case 4:
+      analogWrite(motA1, LOW);
+      digitalWrite(motA2, pwmVa);
+      break;
+  }
 }
-void drive::backward(int pwmVa, int pwmVb) {  //Movimiento hacia atra
-  analogWrite(pwmA, pwmVa);                    //Backwards movement
-  digitalWrite(motA1, LOW);
-  digitalWrite(motA2, HIGH);
-  analogWrite(pwmB, pwmVb);
-  digitalWrite(motB1, LOW);
-  digitalWrite(motB2, HIGH);
+
+void drive::left(int pwmVa, int pwmVb) {  //Drive Leftward / Avanzar a la izquierda
+  switch (type) {
+
+      //2 Motors, PWM via dedicated channel
+
+    case 0:
+      analogWrite(pwmA, pwmVa);
+      digitalWrite(motA1, HIGH);
+      digitalWrite(motA2, LOW);
+      analogWrite(pwmB, pwmVb);
+      digitalWrite(motB1, LOW);
+      digitalWrite(motB2, HIGH);
+      break;
+
+      //2 Motors, No PWM
+
+    case 1:
+      digitalWrite(motA1, HIGH);
+      digitalWrite(motA2, LOW);
+      digitalWrite(motB1, LOW);
+      digitalWrite(motB2, HIGH);
+      break;
+
+      //2 Motors, PWM via enable
+
+    case 2:
+      analogWrite(en1, pwmVa);
+      digitalWrite(motA1, HIGH);
+      digitalWrite(motA2, LOW);
+      analogWrite(en2, pwmVb);
+      digitalWrite(motB1, LOW);
+      digitalWrite(motB2, HIGH);
+      break;
+
+      //2 Motors, No PWM/ PWM via input
+
+    case 3:
+      analogWrite(motA1, pwmVa);
+      digitalWrite(motA2, LOW);
+      analogWrite(motB1, LOW);
+      digitalWrite(motB2, pwmVb);
+      break;
+
+      //1 Motor
+
+    case 4:
+      analogWrite(motA1, pwmVa);
+      digitalWrite(motA2, LOW);
+      break;
+  }
 }
-void drive::left(int pwmVa, int pwmVb) {  //Giro hacia la Izquierda, asumiendo que el motor B esta a la derecha
-  analogWrite(pwmA, pwmVa);                //Left turn, asuming that motor B is on the left side
-  digitalWrite(motA1, HIGH);
-  digitalWrite(motA2, LOW);
-  analogWrite(pwmB, pwmVb);
-  digitalWrite(motB1, LOW);
-  digitalWrite(motB2, HIGH);
+void drive::right(int pwmVa, int pwmVb) {  //Drive Rightward / Avanzar a la Derecha
+  switch (type) {
+
+      //2 Motors, PWM via dedicated channel
+
+    case 0:
+      analogWrite(pwmA, pwmVa);
+      digitalWrite(motA1, LOW);
+      digitalWrite(motA2, HIGH);
+      analogWrite(pwmB, pwmVb);
+      digitalWrite(motB1, HIGH);
+      digitalWrite(motB2, LOW);
+      break;
+
+      //2 Motors, No PWM
+
+    case 1:
+      digitalWrite(motA1, LOW);
+      digitalWrite(motA2, HIGH);
+      digitalWrite(motB1, HIGH);
+      digitalWrite(motB2, LOW);
+      break;
+
+      //2 Motors, PWM via enable
+
+    case 2:
+      analogWrite(en1, pwmVa);
+      digitalWrite(motA1, LOW);
+      digitalWrite(motA2, HIGH);
+      analogWrite(en2, pwmVb);
+      digitalWrite(motB1, HIGH);
+      digitalWrite(motB2, LOW);
+      break;
+
+      //2 Motors, No PWM/ PWM via input
+
+    case 3:
+      analogWrite(motA1, LOW);
+      digitalWrite(motA2, pwmVa);
+      analogWrite(motB1, pwmVb);
+      digitalWrite(motB2, LOW);
+      break;
+
+      //1 Motor
+
+    case 4:
+      analogWrite(motA1, LOW);
+      digitalWrite(motA2, pwmVa);
+      break;
+  }
 }
-void drive::right(int pwmVa, int pwmVb) {  //Giro hacia la derecha, asumiendo que el motor B esta a la derecha
-  analogWrite(pwmA, pwmVa);                 //Right turn, asuming that motor B is on the left side
-  digitalWrite(motA1, LOW);
-  digitalWrite(motA2, HIGH);
-  analogWrite(pwmB, pwmVb);
-  digitalWrite(motB1, HIGH);
-  digitalWrite(motB2, LOW);
+void drive::brake(int pwmVa, int pwmVb) {  //Brake, depending on the driver it can be soft or hard braking. Chek the used drivers datasheet for more info
+  switch (type) {                          //Freno, puede ser suave o repentino dependiendo del controlador. Revisa la hoja de datos del controlador a usar para mas informacion
+
+      //2 Motors, PWM via dedicated channel
+
+    case 0:
+      analogWrite(pwmA, pwmVa);
+      digitalWrite(motA1, HIGH);
+      digitalWrite(motA2, HIGH);
+      analogWrite(pwmB, pwmVb);
+      digitalWrite(motB1, HIGH);
+      digitalWrite(motB2, HIGH);
+      break;
+
+      //2 Motors, No PWM
+
+    case 1:
+      digitalWrite(motA1, LOW);
+      digitalWrite(motA2, LOW);
+      digitalWrite(motB1, LOW);
+      digitalWrite(motB2, LOW);
+      break;
+
+      //2 Motors, PWM via enable
+
+    case 2:
+      digitalWrite(en1, LOW);
+      digitalWrite(motA1, LOW);
+      digitalWrite(motA2, LOW);
+      digitalWrite(en2, LOW);
+      digitalWrite(motB1, LOW);
+      digitalWrite(motB2, LOW);
+      break;
+
+      //2 Motors, No PWM/ PWM via input
+
+    case 3:
+      digitalWrite(motA1, HIGH);
+      digitalWrite(motA2, HIGH);
+      digitalWrite(motB1, HIGH);
+      digitalWrite(motB2, HIGH);
+      break;
+
+      //1 Motor
+
+    case 4:
+      analogWrite(motA1, LOW);
+      digitalWrite(motA2, LOW);
+  }
 }
-void drive::embrake(int pwmVa, int pwmVb) {  //Freno de emergencia. Usar con precaucion, comprobado solo en el controlador TB6612FNG
-  analogWrite(pwmA, pwmVa);                   //Emergency brake. Use with caution, proved only on driver TB6612FNG
-  digitalWrite(motA1, HIGH);
-  digitalWrite(motA2, HIGH);
-  analogWrite(pwmB, pwmVb);
-  digitalWrite(motB1, HIGH);
-  digitalWrite(motB2, HIGH);
+void drive::sleep() {      //Desactiva el Enable o Standby, en algunos controladores esto activa el movimiento libre
+  digitalWrite(en1, LOW);  //Turns of the Enable or Stanby pins, on some drivers this activates the freerun
+  digitalWrite(en2, LOW);
+  digitalWrite(stby, LOW);
 }
-void drive::freerun() {     //Desactiva el Enable o Standby, en algunos controladores esto activa el movimiento libre
-  digitalWrite(stby, LOW);  //Turns of the Enable or Stanby pins, on some drivers this activates the freerun
-}
-void drive::enbrun() {       //Vuelve a activar el Enable o Stanby
-  digitalWrite(stby, HIGH);  //Turns back on the Enable or Stanby pins
+void drive::wakeUp() {       //Vuelve a activar Stanby
+  digitalWrite(stby, HIGH);  //Turns back on the Stanby pin
 }
 void drive::prueba(int pwmVa, int pwmVb) {  //Codigo de prueba, activa en secuencia todas las funciones de movimiento
-  forward(pwmVa, pwmVb);                            //Test code, activates on secuence all the movility functions
+  forward(pwmVa, pwmVb);                    //Test code, activates on secuence all the movility functions
   delay(750);
   backward(pwmVa, pwmVb);
   delay(750);
