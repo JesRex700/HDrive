4a598c682cec105b82011e40947e37ef2eeafa4c
Jesus Amezquita <thomasjesus70@gmail.com>
2026-08-05 10:55:30 -0600

    Update bluethootcar.ino
    Updated to the version 2.0.0 methods

--- examples/bluethootcar/bluethootcar.ino
+++ examples/bluethootcar/bluethootcar.ino
@@ -1,12 +1,14 @@
+// This example uses a Type 0 driver, like the TB612FNG. Also its meant to be used with an arduino nano, uno or mega; with a HC-05 or HC-06 bt modules
+//Este ejemplo usa un driver Type 0, como el TB6612FNG. Tambien esta pensado para ser usado con un arduino nano, uno o mega; usando un modulo bt HC-05 o HC-06
+
 #include <hdrive.h>
 
-drive avanzar(8, 6, 7, 10, 9);
+drive avanzar(6, 7, 10, 9, 8, 5, 11);
 char blue;
 
 void setup() {
   Serial.begin(9600);
   avanzar.init();
-  avanzar.setpwm(5,11);
 }
 
 void loop() {
